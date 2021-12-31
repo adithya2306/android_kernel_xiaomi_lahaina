e2fe425ac90b64e382f9b7419666406e1998c8c3
Yaroslav Furman <yaro330@gmail.com>
2021-12-31 19:05:40 +0300

    thermal: qti: Compile out debugfs driver
     * fails build on newer compilers, and useless
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -2000,7 +2000,9 @@ static int __init thermal_init(void)
 	if (result)
 		pr_warn("Thermal: Can not register suspend notifier, return %d\n",
 			result);
+#ifdef CONFIG_DEBUG_FS
 	thermal_debug_init();
+#endif
 
 	result = of_parse_thermal_message();
 	if (result)
@@ -2037,7 +2039,9 @@ static void thermal_exit(void)
 	genetlink_exit();
 	destroy_thermal_message_node();
 	class_unregister(&thermal_class);
+#ifdef CONFIG_DEBUG_FS
 	thermal_debug_exit();
+#endif
 	thermal_unregister_governors();
 	ida_destroy(&thermal_tz_ida);
 	ida_destroy(&thermal_cdev_ida);
