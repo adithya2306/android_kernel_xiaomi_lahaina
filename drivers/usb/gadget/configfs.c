661b5c825be97a4a3208f7918dee7df8d0003c9b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-14 19:56:55 +0530

    drivers/usb: Import xiaomi changes

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -36,6 +36,11 @@ struct device *create_function_device(char *name)
 EXPORT_SYMBOL_GPL(create_function_device);
 #endif
 
+#undef dev_dbg
+#undef pr_debug
+#define pr_debug pr_err
+#define dev_dbg dev_err
+
 int check_user_usb_string(const char *name,
 		struct usb_gadget_strings *stringtab_dev)
 {
@@ -161,6 +166,7 @@ static int usb_string_copy(const char *s, char **s_copy)
 			return -ENOMEM;
 	}
 	strcpy(str, s);
+
 	if (str[ret - 1] == '\n')
 		str[ret - 1] = '\0';
 	*s_copy = str;
@@ -1672,6 +1678,15 @@ static void configfs_composite_disconnect(struct usb_gadget *gadget)
 	 */
 	acc_disconnect();
 #endif
+
+#ifdef CONFIG_USB_CONFIGFS_F_ACC
+	/*
+	 * accessory HID support can be active while the
+	 * accessory function is not actually enabled,
+	 * so we need to inform it when we are disconnected.
+	 */
+	acc_disconnect();
+#endif
 	gi = container_of(cdev, struct gadget_info, cdev);
 	spin_lock_irqsave(&gi->spinlock, flags);
 	cdev = get_gadget_data(gadget);
@@ -1680,11 +1695,13 @@ static void configfs_composite_disconnect(struct usb_gadget *gadget)
 		WARN(1, "%s: gadget driver already disconnected\n", __func__);
 		return;
 	}
+
 #ifdef CONFIG_USB_CONFIGFS_UEVENT
 	gi->connected = false;
 	if (!gi->unbinding)
 		schedule_work(&gi->work);
 #endif
+
 	composite_disconnect(gadget);
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
