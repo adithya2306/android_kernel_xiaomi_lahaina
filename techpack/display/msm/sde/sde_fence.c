a306cae617fe32532b239952a5087ed127d4a5ed
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-02-09 14:58:25 +0530

    Revert "techpack: display: Add some bp hints to hot paths"
    This reverts commit cac674fd641ee3c16842be0ce58bac5fa933bfc5.

--- techpack/display/msm/sde/sde_fence.c
+++ techpack/display/msm/sde/sde_fence.c
@@ -190,13 +190,13 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 	signed int fd = -EINVAL;
 	struct sde_fence_context *ctx = fence_ctx;
 
-	if (unlikely(!ctx)) {
+	if (!ctx) {
 		SDE_ERROR("invalid context\n");
 		goto exit;
 	}
 
 	sde_fence = kmem_cache_zalloc(kmem_fence_pool, GFP_KERNEL);
-	if (unlikely(!sde_fence))
+	if (!sde_fence)
 		return -ENOMEM;
 
 	sde_fence->ctx = fence_ctx;
@@ -206,14 +206,14 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 
 	/* create fd */
 	fd = get_unused_fd_flags(0);
-	if (unlikely(fd < 0)) {
+	if (fd < 0) {
 		dma_fence_put(&sde_fence->base);
 		goto exit;
 	}
 
 	/* create fence */
 	sync_file = sync_file_create(&sde_fence->base);
-	if (unlikely(sync_file == NULL)) {
+	if (sync_file == NULL) {
 		put_unused_fd(fd);
 		fd = -EINVAL;
 		dma_fence_put(&sde_fence->base);
@@ -319,7 +319,7 @@ int sde_fence_create(struct sde_fence_context *ctx, uint64_t *val,
 	int fd, rc = -EINVAL;
 	unsigned long flags;
 
-	if (unlikely(!ctx || !val)) {
+	if (!ctx || !val) {
 		SDE_ERROR("invalid argument(s), fence %d, pval %d\n",
 				ctx != NULL, val != NULL);
 		return rc;
